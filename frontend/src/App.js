0105b950b33530f7013230e4a6c4f3070ac87cb3
Christian <christian.pietropaulo.nobile@gmail.com>
2025-07-04 02:59:28 +0200

    build

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to SmartVault</h1>
+      <p>Your secure, decentralized password manager.</p>
+    </div>
+  );
+}
+
+export default App;
